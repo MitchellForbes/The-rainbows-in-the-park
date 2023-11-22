f17dbbbfef87fc407fb18e7d9f08e26f5540edd8
MitchellForbes <135566058+MitchellForbes@users.noreply.github.com>
2023-11-22 16:03:36 +1100

    Bullet destroyed on hitting bomb

--- Stuff/Bomb.cs
+++ Stuff/Bomb.cs
@@ -4,7 +4,7 @@ using System;
 public partial class Bomb : Area3D
 {
 	
-	float health = 3;
+	float health = 4;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -23,10 +23,12 @@ public partial class Bomb : Area3D
 	
 	void _on_body_entered(Node3D body)
 	{	
+		
 		if (body != null && body.IsInGroup("Bullets"))
 		{
 			health -= 1;
 			GD.Print(health);
+			body.QueueFree();
 		}
 	}
 }
